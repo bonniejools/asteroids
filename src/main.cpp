456870b6dd43a4348399a1a3798a24203e182e82
Jonathan Balls <j.balls@btopenworld.com>
2016-12-07 04:26:32 +0000

    Set indentation of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,61 +14,61 @@
 
 int main()
 {
-	using namespace std;
+    using namespace std;
 
-	// Creates the GLFW context.
-	// GLFW creates a window with an OpenGL context.
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	GLFWwindow* window = glfwCreateWindow(WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE, nullptr,
-		nullptr);
-	glfwMakeContextCurrent(window);
+    // Creates the GLFW context.
+    // GLFW creates a window with an OpenGL context.
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE, nullptr,
+            nullptr);
+    glfwMakeContextCurrent(window);
 
-	// GLEW links to gpu functions
-	glewExperimental = GL_TRUE;
-	glewInit();
+    // GLEW links to gpu functions
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-	// Loads vertex & fragment shaders
-	GLuint program = LoadShader(VERTEX_SHADER_LOCATION,
-		FRAGMENT_SHADER_LOCATION);
-	glUseProgram(program);
+    // Loads vertex & fragment shaders
+    GLuint program = LoadShader(VERTEX_SHADER_LOCATION,
+            FRAGMENT_SHADER_LOCATION);
+    glUseProgram(program);
 
-	// Make vbo the current object with glBindBuffer() and then 'upload' data to it.
-	// GL_STATIC_DRAW means that the vertex data will be uploaded once and then
-	// drawn lots and lots of times.
+    // Make vbo the current object with glBindBuffer() and then 'upload' data to it.
+    // GL_STATIC_DRAW means that the vertex data will be uploaded once and then
+    // drawn lots and lots of times.
 
-        Asteroid asteroid = Asteroid(program, 3);
-        Player player = Player(program);
+    Asteroid asteroid = Asteroid(program, 3);
+    Player player = Player(program);
 
-	while(!glfwWindowShouldClose(window))
-	{
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-		// Close window if 'esc' key is pressed
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        // Close window if 'esc' key is pressed
 
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-			glfwSetWindowShouldClose(window, GL_TRUE);
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GL_TRUE);
 
-		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-                        player.Rotate(0.1);
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+            player.Rotate(0.1);
 
-		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-                        player.Rotate(-0.1);
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+            player.Rotate(-0.1);
 
-		// Clear the screen to black
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-                player.Draw();
-                asteroid.Draw();
+        player.Draw();
+        asteroid.Draw();
 
-	}
+    }
 
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return 0;
 }
 
