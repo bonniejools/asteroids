1b0374fb40884d09bf4256ea576174dd4e8f0567
Jonathan Balls <j.balls@btopenworld.com>
2016-12-10 10:45:59 +0000

    Add camera

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,10 +3,13 @@
 in vec2 position;
 
 uniform mat4 scale;
-uniform mat4 model;
+uniform mat4 model; // Model should be rotation * scale * translation
 uniform mat4 trans;
+
+uniform mat4 view;
+uniform mat4 proj;
 
 void main()
 {
-    gl_Position = trans * scale * model * vec4(position.x, position.y , 0.0, 1.0);
+    gl_Position = proj * view * trans * scale * model * vec4(position.x, position.y , 0.0, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -41,16 +41,29 @@ int main()
     glewInit();
 
     // Loads vertex & fragment shaders
-    GLuint program = LoadShader(VERTEX_SHADER_LOCATION,
+    GLuint shaderProgram= LoadShader(VERTEX_SHADER_LOCATION,
             FRAGMENT_SHADER_LOCATION);
-    glUseProgram(program);
+    glUseProgram(shaderProgram);
 
     // Seed the random number and generate asteroids.
     Asteroid *asteroids = (Asteroid *) malloc(NUMBER_OF_ASTEROIDS * sizeof(Asteroid));
     for (int i=0; i<NUMBER_OF_ASTEROIDS; i++) {
-        asteroids[i] = Asteroid(program, 3);
+        asteroids[i] = Asteroid(shaderProgram, 3);
     }
-    Player player = Player(program);
+    Player player = Player(shaderProgram);
+
+    // Create the camera
+    glm::mat4 view = glm::lookAt(
+                glm::vec3(1.2f, 1.2f, 1.2f),
+                    glm::vec3(0.0f, 0.0f, 0.0f),
+                        glm::vec3(0.0f, 0.0f, 1.0f)
+            );
+    GLint uniView = glGetUniformLocation(shaderProgram, "view");
+    glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+
+    glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 1.0f, 10.0f);
+    GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+    glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
 
     auto last_frame_time = std::chrono::high_resolution_clock::now();
 
